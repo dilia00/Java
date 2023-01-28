42aae5777aca3cd13ad6af8aa8c014b663b653f0
dilia00 <dina_xenia@mail.ru>
2023-01-28 21:16:58 +0300

    Синтаксис java

---
+++ Lesson_1/program.java
@@ -0,0 +1,14 @@
+package Lesson_1;
+
+/**
+ * program
+ */
+public class program {
+
+    public static void main(String[] args) {
+        // int a = 123;
+        // a = --a - a--;
+        // System.out.println(a);
+
+    }
+}
